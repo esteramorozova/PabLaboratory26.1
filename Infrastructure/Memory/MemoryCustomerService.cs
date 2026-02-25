2d7976dadaa8f8f4c893665c4abb6c1667aee9d5
estera.morozova <asiy2001@ukr.net>
2026-02-25 11:33:28 +0100

    end of lab1

---
+++ Infrastructure/Memory/MemoryCustomerService.cs
@@ -0,0 +1,36 @@
+﻿using AppCore.Interfaces;
+using AppCore.Models;
+
+namespace Infrastructure.Memory;
+
+public class MemoryCustomerService: ICustomerService
+{
+    public IEnumerable<Customer> GetCustomers()
+    {
+        return [
+        new Customer()
+            {
+                Id = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                Email = "a@wsei.edu.pl",
+                Phone = "123-456-789",
+                AddressId = 11
+            },
+            new Customer()
+                {
+                    Id = 2,
+                    FirstName = "Anna",
+                    LastName = "Nowak",
+                    Email = "b@wsei.edu.pl",
+                    Phone = "987-654-321",
+                    AddressId = 22
+                }
+            ];
+    }
+
+    public Task<IEnumerable<Customer>> GetCustomersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
